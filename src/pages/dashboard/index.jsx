00bebb68ee2ea148e29b80c48c471b61bed01540
delafjr <dela.fjr08@gmail.com>
2025-05-10 16:16:08 +0700

    fix: play quiz ui

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -37,7 +37,7 @@ const Dashboard = () => {
     }, [])
 
     const playQuiz = (quizId) => {
-        navigate(`/play-quiz/${quizId}/question/1`)
+        navigate(`/play-quiz/${quizId}/question/`)
     }
 
     return(
--- src/pages/play-quiz/index.jsx
+++ src/pages/play-quiz/index.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { useAuth } from "../../hooks/AuthContext"
 import axios from "axios"
+import Navbar from "../../components/Navbar"
 
 const PlayQuiz = () => {
     const {url, token} = useAuth()
     let { quizId } = useParams()
+    const [quizName, setQuizName] = useState('')
     const [questions, setQuestions] = useState([])
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
     const [selectedAnswer, setSelectedAnswer] = useState(null)
@@ -20,9 +22,9 @@ const PlayQuiz = () => {
                     'Authorization': 'bearer '+ token
                 }
             }).then((response) => {
-                const result = response.data.payload.datas.questions
-                setQuestions(result)
-                console.log(response.data.payload.datas.questions)
+                const result = response.data.payload.datas
+                setQuizName(result.title)
+                setQuestions(result.questions)
             }).catch(error => {
                 console.log(error)
             })
@@ -51,27 +53,49 @@ const PlayQuiz = () => {
     const currentQuestion = questions[currentQuestionIndex]
 
     return(
-        <div>
-            <h1>Soal {currentQuestionIndex + 1}</h1>
-            <h3>{currentQuestion.question}</h3>
-            {currentQuestion.choices.map(choice => (
-                <div key={choice.id}>
-                    <input 
-                        type="radio" 
-                        name={`question-${currentQuestion.id}`}
-                        value={choice.id}
-                        onChange={() => handleAnswerChange(choice.id)}
-                        checked={selectedAnswer === choice.id}
-                    />
-                    <label>{choice.choice}</label>
+        <div className="min-h-screen flex flex-col text-black">
+            <Navbar/>
+            <div className="w-full lg:w-1/3 mx-auto my-2 px-5 mt-20">
+                <div className="w-full flex place-content-between text-black font-semibold">
+                    <h1 className="text-xl">
+                        {quizName}
+                    </h1>
+                    <div className="border-border border-2 py-1 px-3 rounded-[50%]">
+                        {currentQuestionIndex + 1}
+                    </div>
                 </div>
-            ))}
-            <button
-                onClick={submitAnswer}
-                disabled={isSubmitting || selectedAnswer === null}    
-            >
-                {isSubmitting ? 'Menyimpan' : 'Kirim Jawaban'}
-            </button>
+                <h3 className="my-2">
+                    {currentQuestion.question}
+                </h3>
+                {currentQuestion.choices.map(choice => (
+                    <div 
+                        key={choice.id}
+                        className="border-border rounded-md border-1 p-3 my-2"
+                    >
+                            <input 
+                                type="radio" 
+                                name={`question-${currentQuestion.id}`}
+                                value={choice.id}
+                                onChange={() => handleAnswerChange(choice.id)}
+                                checked={selectedAnswer === choice.id}
+                            />
+                            <label className="px-2">
+                                {choice.choice}
+                            </label>
+                    </div>
+                ))}
+                <div className="fixed bottom-0 w-full bg-white border-t border-gray-200 ">
+                    <div className=" w-full lg:w-1/3 h-full mx-auto">
+                        <button
+                            onClick={submitAnswer}
+                            disabled={isSubmitting || selectedAnswer === null} 
+                            className="w-full bg-primary text-white  my-2 rounded-md py-2"   
+                        >
+                            {isSubmitting ? 'Menyimpan' : 'Kirim Jawaban'}
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
