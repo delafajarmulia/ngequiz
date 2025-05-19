1e9f58d54b2219ecfb6da388e0f3e74ec0004399
delafjr <dela.fjr08@gmail.com>
2025-05-19 22:53:33 +0700

    fix: change opacity in tablayout

--- src/components/TabLayout.jsx
+++ src/components/TabLayout.jsx
@@ -4,7 +4,7 @@ import useNavTabLayout from "../hooks/UseNavTabLayout"
 export const TabLayout = () => {
     const {isDashboardActive, isLeaderboardActive} = useNavTabLayout()
     return(
-        <div className="w-full mx-auto bg-primary opacity-60 rounded-md p-3">
+        <div className="w-full mx-auto bg-primary opacity-70 rounded-md p-3">
             <div className="grid grid-cols-2 text-white gap-5 text-center font-semibold">
                 {isDashboardActive ? (
                     <div className="bg-white text-primary py-1.5 rounded-md opacity-100">
